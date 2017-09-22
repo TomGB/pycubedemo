ceb8b61d5b0f399f9935ab45489b03234c7134f2
Tom Banister <tgbanister@gmail.com>
2017-09-22 15:45:38 +0100

    fixed matrix

--- patterns/matrix.py
+++ patterns/matrix.py
@@ -9,31 +9,29 @@ import math
 class Pattern(object):
     def init(self):
         self.double_buffer = True
-
         self.items = []
+        self.startStrength = 8
 
         return 1.0/10
 
     def trySetPixel(self, pos, colour):
-        if pos[0] >= 0 and pos[0] < 8 and pos[1] >= 0 and pos[1] < 8 and pos[2] >= 0 and pos[2] < 8 :
+        if pos[0] >= 0 and pos[0] < 8 and pos[1] >= 0 and pos[1] < 8 and pos[2] >= 0 and pos[2] < self.startStrength :
             self.cube.set_pixel(pos, colour)
     def tick(self):
         self.cube.clear()
 
-        newItem = type('obj', (object,), { 'pos': (random.randint(0, 8), random.randint(0, 8), 7), 'speed': 10, 'strength': 8 })
+        newItem = type('obj', (object,), { 'pos': (random.randint(0, 8), random.randint(0, 8), 7), 'speed': 10, 'strength': self.startStrength })
 
         self.items.append(newItem)
 
-        mainColor = (0, 255, 0)
-        secondColor = (200, 200, 200)
-        thirdColor = (100, 100, 100)
-
         for item in self.items:
-            self.trySetPixel(item.pos, mainColor)
+            self.trySetPixel(item.pos, (0, 255 * item.strength / self.startStrength, 0))
 
-        for item in self.items:
-            if item.strength > 0 :
+        for item in reversed(self.items):
+            if item.strength > 0:
                 item.pos = (item.pos[0], item.pos[1], item.pos[2] - 1)
+            if item.strength > 0 and item.pos[2] == 6:
                 newItem = type('obj', (object,), { 'pos': (item.pos[0], item.pos[1], item.pos[2] + 1), 'speed': item.speed, 'strength': item.strength - 1 })
-            if item.pos[2] <0 :
-                items.remove(item)
+                self.items.append(newItem)
+            if item.pos[2] < 0 or item.strength <= 0:
+                self.items.remove(item)
